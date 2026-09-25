ad249ba96a3e4fb671b5f76880c616c6bca1c910
rolmerdiaz <rolmerdiaz@gmail.com>
2026-09-25 10:32:21 -0400

    Update app.py

--- app.py
+++ app.py
@@ -241,5 +241,5 @@ def handle_iniciar_proceso(json_data):
 
 
 if __name__ == '__main__':
-    # Ejecución local limpia en el puerto 5000
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port)
