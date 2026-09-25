a61e14f3b3bfed84ecc3f63dff753213b176fd4d
rolmerdiaz <rolmerdiaz@gmail.com>
2026-09-25 10:43:14 -0400

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.actionchains import ActionChains
+from selenium.webdriver.common.action_chains import ActionChains
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret_key_rolmer_2026'
