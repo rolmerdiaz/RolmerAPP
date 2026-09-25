7b994a5c8b6a0b97433914cb6c55b0a7e0034523
rolmerdiaz <rolmerdiaz@gmail.com>
2026-09-25 11:01:52 -0400

    Update app.py

--- app.py
+++ app.py
@@ -31,6 +31,8 @@ def crear_driver():
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
+    options.add_argument("--disable-software-rasterizer")
+    options.add_argument("--remote-debugging-port=9222")
     options.add_argument("--window-size=1920,1080")
 
     driver = webdriver.Chrome(options=options)
